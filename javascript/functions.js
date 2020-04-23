ecc24d4ff37e7080bf4530cad71cb10dcf5cc352
liron <lironoskar@gmail.com>
2020-04-23 21:11:06 +0300

    regiter, login, about

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,158 +1,201 @@
-
-$(document).ready(function() {
-    $("#welcomeDiv").show();
-    $("#loginDiv").hide();
-    $("#registerDiv").hide();
-    $("#aboutDiv").hide();
-    $("#canvesDiv").hide()
-});
-$.validator.setDefaults({
-    submitHandler: function() {
-        alert("submitted!");
-    }
-});
+var user = {
+  firstname:"",
+  lastName:"",
+  username: "",
+  password: "",
+  email:"",
+  birth:""
+};
+var users =[];
+var defaultUser = {firstname:"p", lastName:"p",username:"p",password:"p",email:"p@example.com",birth:"1/1/1990"};
+users.push(defaultUser);
 /*
-registration validation using the jquery validation plugin
-*/
-$().ready(function () {
-    $("#signupForm").validate({
-      rules: {
-        firstName:{ 
-          required: true,
-          lettersonly: true
-        },
-        lastName:{
-          required: true,
-          lettersonly: true
-        },
-        password:{
-          required: true,
-          minlength:8,
-          numAndLetter:true
-        },
-        email:{
-          validEmail:true
-        },
-        username:{
-          required:true
-        },
-        birth:{
-          required:true,
-          legalDate: true
-        }
-      },
-      messages: {
-        firstName:{
-          required: "Please enter your first name"
-        },
-        lastName:{
-          required: "Please enter your last name"
-        },
-        password:{
-          required: "Please enter a password",
-          minlength:"The password must be at least 8 characters long",
-          numAndLetter:"Your password must contain at least one letter and one number"
-        },
-        email:{
-          email:"Please insert a valid email address"
-        },
-        username:{
-          required: "Please enter username"
-        },
-        birth:{
-          required: "Please enter your date of birth",
-          legalDate: "Please enter a date that is before today"
-        }
-      },
-      submitHandler: function(form,event){
-        var newUser = Object.create(user);
-        var temp = $("#username");
-        newUser.username = $("#username").val();
-        newUser.password = $("#password").val();
-        newUser.firstName = $("#firstName").val();
-        newUser.lastName = $("#lastName").val();
-        newUser.email = $("#email").val();
-        newUser.birth = $("#birth").val();
-        users.push(newUser);
-        clearFields();
-        alert("User has been created");
-        event.preventDefault();
-        
-      }
-    });
-  });
-  /*
-method definition for jquery validation plugin.
-validates that the input value contains at least one letter and one number
-*/
-jQuery.validator.addMethod("numAndLetter", function(value, element) {
-    var chars = /[a-zA-Z]/g;
-    var numbers = /[0-9]/g;
-    return chars.test(value) && numbers.test(value);
-  }, "Password must contain at least one letter and one number");
-  
-  /*
-  method definition for jquery validation plugin.
-  validates that the input value is valid email address
-  */
-  jQuery.validator.addMethod("validEmail", function(value, element) {
-    var emailPattern =  /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/g;
-    return emailPattern.test(value);
-  }, "Invalid Email address");
-  
-  /*
-  method definition for jquery validation plugin.
-  validates that the input value contains only letters
-  */
-  jQuery.validator.addMethod("lettersonly", function(value, element) {
-    var numbers = /[0-9]/g;
-    return !numbers.test(value);
-  }, "Your name should contain only letter");
-  
-  /*
-    method definition for jquery validation plugin.
-    validates that the date input is before today
-  */
-  jQuery.validator.addMethod("legalDate", function(value, element) {
-    var enteredDate = new Date(value);
-    var ageDifMs = Date.now() - enteredDate.getTime();
-      return ageDifMs>0;
-  }, "Please enter a date that is before today");
-
+var users = {};
+users["p"] = "p";*/
+/* */
 function welcome(){
-    $("#welcomeDiv").show();
-    $("#loginDiv").hide();
-    $("#registerDiv").hide();
-    $("#aboutDiv").hide();
-    /*$("#game").hide();*/
-    /*$("#userSettings").hide();*/
+  $("#welcomeDiv").show();
+  $("#loginDiv").hide();
+  $("#registerDiv").hide();
 }
 
 function login(){
-    $("#welcomeDiv").hide();
-    $("#loginDiv").show();
-    $("#registerDiv").hide();
-    $("#aboutDiv").hide();
-    /*$("#game").hide();*/
-    /*$("#userSettings").hide();*/
+  $("#welcomeDiv").hide();
+  $("#loginDiv").show();
+  $("#registerDiv").hide();
 }
 
 function register(){
-    $("#welcomeDiv").hide();
-    $("#loginDiv").hide();
-    $("#registerDiv").show();
-    $("#aboutDiv").hide();
-    /*$("#game").hide();*/
-    /*$("#userSettings").hide();*/
+  $("#welcomeDiv").hide();
+  $("#loginDiv").hide();
+  $("#registerDiv").show();
 }
 
+/*login*/
+function loginButton(){
+  var userCheck=document.getElementById("userName").value;
+  var passwordCheck=document.getElementById("passWord").value;
+  alert(userCheck);
+  alert(users.length);
+  for(var i=0;i<users.length;i++){
+    if(users[i].username===userCheck){
+      if(users[i].password === passwordCheck){
+        user=users[i];
+        $("#loginDiv").hide();
+        $("#userSettings").show();
+        alert("Succeeded")
+        return;
+      }
+      else
+        alert("Wrong password. \nPlease try again");
+        $("loginDiv").show();
+        return;
+    }
+  }
+  alert("User does not exist in the system.");
+/*
+  if(!(userCheck in users)){
+      alert("username does not exist")
+          return;
+      }
+  else{
+      if (users[userCheck] == passwordCheck) {
+          user=userCheck;
+          $("#loginDiv").hide();
+          $("#userSettings").show();
+      }
+      else{
+        alert("Password is invalid. \nPlease try again");
+          return;
+      }
+  }*/
+}
 
-function about(){
-    $("#welcomeDiv").hide();
+/*start page with welcome */
+$(document).ready(function start() {
+    $("#welcomeDiv").show();
     $("#loginDiv").hide();
     $("#registerDiv").hide();
-    $("#aboutDiv").show();
-    /*$("#game").hide();*/
-    /*$("#userSettings").hide();*/
-}
+    $("#canvesDiv").hide()
+});
+/*exit from Model Dialog with esc*/
+$(document).keydown(function(e) { 
+  if (e.keyCode == 27) { 
+      $("#closeModel").click();
+  } 
+});
+
+/*form of registration*/
+$.validator.setDefaults({
+  submitHandler: function() {
+    alert("submitted!");
+  }
+});
+
+$().ready(function() {
+  // validate the comment form when it is submitted
+  $("#commentForm").validate();
+
+  // validate signup form on keyup and submit
+  $("#signupForm").validate({
+    rules: {
+      firstname: {
+        required: true,
+        checkName: true
+      },
+      lastname: {
+        required: true,
+        checkName: true
+      },
+      username: {
+        required: true,
+        minlength: 2
+      },
+      password: {
+        required: true,
+        minlength: 6,
+        checkPassword: true
+      },
+      confirm_password: {
+        required: true,
+        minlength: 6,
+        equalTo: "#password"
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      birth: {
+        required: true,
+        checkDate: true
+      }
+    },
+    messages: {
+      firstname: {
+        required: "Please enter your first name",
+        checkName: "Your name should contain only letter"
+        
+      },
+      lastname: {
+        required: "Please enter your last name",
+        checkName: "Your name should contain only letter"
+      },
+      username: {
+        required: "Please enter a username",
+        minlength: "Your username must consist of at least 2 characters"
+      },
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 6 characters long",
+        checkPassword: "Password must contain at least one letter and one number"
+      },
+      confirm_password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 6 characters long",
+        equalTo: "Please enter the same password as above"
+      },
+      birth: {
+        required: "Pleasr enter a birthday",
+        checkDate: "Please enter a valid date"
+      },
+      email: "Please enter a valid email address",
+    },
+    submitHandler: function(form,event){
+      var newUser = Object.create(user);
+      newUser.firstName = $("#firstName").val();
+      newUser.lastName = $("#lastName").val();
+      newUser.username = $("#username").val();
+      newUser.password = $("#password").val();
+      newUser.email = $("#email").val();
+      newUser.birth = $("#birth").val();
+      users.push(newUser);
+      alert(newUser.username+" has been created ");
+      clearFields();
+    }
+  });
+  // propose username by combining first- and lastname
+  /*
+  $("#username").focus(function() {
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    if (firstname && lastname && !this.value) {
+      this.value = firstname + "." + lastname;
+    }
+  });*/
+});
+   /*functions for validation data in registrtion*/
+  /*valid password - at least one letter and one number*/
+jQuery.validator.addMethod("checkPassword", function(value, element) {
+  return /^(?=.*[a-zA-Z])(?=.*\d).*$/.test(value) && /^[0-9a-zA-Z]+$/.test(value);
+}),
+
+/*valid first name and last name*/
+jQuery.validator.addMethod("checkName", function(value, element) {
+  return /^[^0-9]+$/.test(value);
+}),
+
+/*legal date*/
+jQuery.validator.addMethod("checkDate", function(value, element) {
+    return Date.now() - new Date(value).getTime()>0;
+});
 
